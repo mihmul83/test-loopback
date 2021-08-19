79bfcc8d7899f263ca07a789736331d8b75389b1
Mike Mulyukin <mulukin1983@mail.ru>
2021-08-19 15:14:31 +0400

    ini

--- build/server/boot/models-automigration.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-module.exports = function (app) {
-    console.log('-- Models found:', Object.keys(app.models));
-    app.dataSources.Postgre.autoupdate(app.models, () => {
-        console.log("Auto-migrated models successfully.");
-    });
-};
-//# sourceMappingURL=models-automigration.js.map
